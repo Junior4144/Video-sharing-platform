6555a51f5f3de7bf7e3a82a84b120ce9a0e36775
Junior4144 <94208651+Junior4144@users.noreply.github.com>
2024-09-05 21:45:09 -0500

    applied video- processing-service that uses cloud run, docker, pub/sub. applied yt-web-client that uses ne xt.js

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvmMKFim12rG_pWdMXLdEnDrR5NiXJd0E",
+  authDomain: "yt-clone-17724.firebaseapp.com",
+  projectId: "yt-clone-17724",
+  appId: "1:367938497165:web:cbf297acb32ec98c9710c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+  return onAuthStateChanged(auth, callback);
+}
